9c94b6068684eaa130519427db527aa49a1e7e22
rajeswari-suni <rajeswarisuni@gmail.com>
2026-03-07 15:31:22 +0530

    Add project files.

---
+++ ClothingStore Project/Controllers/LoginController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ClothingStore_Project.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Verify(string mobileNumber)
+        {
+            ViewBag.MobileNumber = mobileNumber;
+            return RedirectToAction("RoleSelection");
+        }
+
+        public IActionResult RoleSelection()
+        {
+            return View();
+        }
+        public IActionResult Brands()
+        {
+            return View();
+        }
+        public IActionResult Products(string brand)
+        {
+            ViewBag.Brand = brand;
+            return View();
+        }
+    }
+    }
