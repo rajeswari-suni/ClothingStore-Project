b567ac2234d2e3b923bf61d902f50310c8b06f94
rajeswari-suni <rajeswarisuni@gmail.com>
2026-03-08 04:18:46 +0530

    Created Product Model

---
+++ ClothingStore Project/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿namespace ClothingStore_Project.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+
+        public string ProductName { get; set; }
+
+        public int Price { get; set; }
+
+        public string Brand { get; set; }
+
+        public string ImageUrl { get; set; }
+    }
+}
